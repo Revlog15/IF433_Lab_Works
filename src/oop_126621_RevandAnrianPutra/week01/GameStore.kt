5aac6d4ca536c346ab53aead1a26b336f5d26ae8
Revlog15 <revand.anrian.putra@gmail.com>
2026-02-04 23:53:19 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_126621_RevandAnrianPutra/week01/GameStore.kt
+++ src/oop_126621_RevandAnrianPutra/week01/GameStore.kt
@@ -4,3 +4,6 @@ fun main() {
     val gameTitle = "Cyberpunk 2077"
     val price = 600000
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) price * 80 / 100 else price * 90 / 100
