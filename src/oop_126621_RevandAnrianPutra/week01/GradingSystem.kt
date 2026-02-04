83530e4b737a7a554c4bfaa89685c8b7640fd81d
Revlog15 <revand.anrian.putra@gmail.com>
2026-02-04 23:44:21 +0700

    week01: add calculateStatus function

--- src/oop_126621_RevandAnrianPutra/week01/GradingSystem.kt
+++ src/oop_126621_RevandAnrianPutra/week01/GradingSystem.kt
@@ -2,17 +2,19 @@ package oop_126621_RevandAnrianPutra.week01
 fun main() {
     val name = "john Thor"
     val score = 80
-
-
-    println("Nama:  $name  , Nilai: $score")
-// Tambahkan di bawah println sebelumnya
-    val grade = when (score) {
-        in 90..100 -> "A"
-        in 80..89 -> "B"
-        in 70..79 -> "C"
-        else -> "D"
-    }
-
+    val grade = calculateGrade(score)
     println("Grade kamu: $grade")
-
+    println("Nama:  $name  , Nilai: $score")
 }
+    fun calculateGrade(score: Int): String =
+        when (score) {
+            in 90..100 -> "A"
+            in 80..89 -> "B"
+            in 70..79 -> "C"
+            else -> "D"
+        }
+
+
+
+
+
