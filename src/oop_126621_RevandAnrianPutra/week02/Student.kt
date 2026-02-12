cbff91cceb4500a9e7d32f0a4682b5d8b2e1828a
Revlog15 <revand.anrian.putra@gmail.com>
2026-02-12 09:44:27 +0700

    week02: add warning validation in init block

--- src/oop_126621_RevandAnrianPutra/week02/Student.kt
+++ src/oop_126621_RevandAnrianPutra/week02/Student.kt
@@ -4,4 +4,16 @@ class Student(
     val nim: String,
     var name: String,
     var major: String
-)
+) {
+
+    init {
+        if (nim.length != 10) {
+            println("WARNING: NIM tidak valid (harus 10 digit)")
+        }
+
+        if (name.isBlank()) {
+            println("WARNING: Nama tidak boleh kosong")
+        }
+    }
+}
+
