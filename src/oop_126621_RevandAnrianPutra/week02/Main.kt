704d995bf981b2c43e2708a0910424dd868a4f32
Revlog15 <revand.anrian.putra@gmail.com>
2026-02-12 10:17:48 +0700

    week02: (task 1) implement library input system

--- src/oop_126621_RevandAnrianPutra/week02/Main.kt
+++ src/oop_126621_RevandAnrianPutra/week02/Main.kt
@@ -39,6 +39,31 @@ fun main() {
     } else {
         println("Pilihan ngawur, pendaftaran batal!")
     }
+    println()
+    println("--- SISTEM PEMINJAMAN BUKU ---")
+
+    print("Judul Buku: ")
+    val title = scanner.nextLine()
+
+    print("Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Lama Pinjam (hari): ")
+    var duration = scanner.nextInt()
+    scanner.nextLine()
+
+    if (duration < 0) {
+        duration = 1
+    }
+
+    val loan = Loan(title, borrower, duration)
+
+    println("Detail Peminjaman:")
+    println("Buku: ${loan.bookTitle}")
+    println("Peminjam: ${loan.borrower}")
+    println("Durasi: ${loan.loanDuration} hari")
+    println("Total Denda: Rp ${loan.calculateFine()}")
+
 }
 
 
