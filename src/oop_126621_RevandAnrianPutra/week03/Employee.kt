34ac24a16dc85d7e9f55508e922cb55caf83a9de
Revlog15 <revand.anrian.putra@gmail.com>
2026-02-18 15:23:34 +0700

    week03: fix setter using backing field

--- src/oop_126621_RevandAnrianPutra/week03/Employee.kt
+++ src/oop_126621_RevandAnrianPutra/week03/Employee.kt
@@ -1,5 +1,15 @@
 package oop_126621_RevandAnrianPutra.week03
 
+
+
 class Employee(val name: String) {
+
     var salary: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif")
+            } else {
+                field = value
+            }
+        }
 }
--- src/oop_126621_RevandAnrianPutra/week03/Main.kt
+++ src/oop_126621_RevandAnrianPutra/week03/Main.kt
@@ -1,2 +1,7 @@
 package oop_126621_RevandAnrianPutra.week03
 
+fun main() {
+    val e = Employee("Budi")
+    println("Set Error")
+    e.salary = 5000
+}
