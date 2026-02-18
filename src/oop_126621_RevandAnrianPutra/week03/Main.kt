6a113594285a52b51f5b6359f92b6dcdcf2357d7
Revlog15 <revand.anrian.putra@gmail.com>
2026-02-18 15:52:43 +0700

    week03: cleanup code

--- src/oop_126621_RevandAnrianPutra/week03/Main.kt
+++ src/oop_126621_RevandAnrianPutra/week03/Main.kt
@@ -3,7 +3,7 @@ package oop_126621_RevandAnrianPutra.week03
 fun main() {
 
     val e = Employee("Budi")
-    e.salary = -1000
+
     e.salary = 5000
     println("Gaji: ${e.salary}")
     println("Net salary: ${e.netSalary}")
