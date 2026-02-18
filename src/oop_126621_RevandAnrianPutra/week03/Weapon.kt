51af9ccf290311eac4cb42aeb8d548c56553756c
Revlog15 <revand.anrian.putra@gmail.com>
2026-02-18 15:41:36 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_126621_RevandAnrianPutra/week03/Weapon.kt
@@ -0,0 +1,24 @@
+package oop_126621_RevandAnrianPutra.week03
+
+class Weapon(val name: String, damageInit: Int) {
+
+    var damage: Int = damageInit
+        set(value) {
+            when {
+                value < 0 -> {
+                    println("Damage tidak boleh negatif")
+                }
+                value > 1000 -> {
+                    field = 1000
+                }
+                else -> field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
