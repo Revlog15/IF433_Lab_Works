212cbecf50ac314e029e86e88410d0ddb6efa4ff
Revlog15 <revand.anrian.putra@gmail.com>
2026-03-11 15:17:46 +0700

    week06: create Watch abstract class

--- src/oop_126621_RevandAnrianPutra/week06/Button.kt
+++ src/oop_126621_RevandAnrianPutra/week06/Button.kt
@@ -1,2 +1,7 @@
 package oop_126621_RevandAnrianPutra.week06
 
+class Button(override val name: String) : Clickable {
+    override fun click() {
+        println("Tombol '$name' berhasil diklik!")
+    }
+}
---
+++ src/oop_126621_RevandAnrianPutra/week06/Watch.kt
@@ -0,0 +1,5 @@
+package oop_126621_RevandAnrianPutra.week06
+
+abstract class Watch {
+    abstract fun showTime()
+}
