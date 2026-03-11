3524c911dcba64430a4bd2c19374ba56d055b829
Revlog15 <revand.anrian.putra@gmail.com>
2026-03-11 15:26:27 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_126621_RevandAnrianPutra/week06/SmartSpeaker.kt
@@ -0,0 +1,15 @@
+package oop_126621_RevandAnrianPutra.week06
+
+class SmartSpeaker(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("$name menyala.")
+    }
+
+    override fun turnOff() {
+        println("$name dimatikan.")
+    }
+
+    fun playMusic(song: String) {
+        println("Memutar lagu $song dari Spotify.")
+    }
+}
