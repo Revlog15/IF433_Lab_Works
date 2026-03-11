981c54cfc481f92beedbdedfe40a5d3ea189f4a0
Revlog15 <revand.anrian.putra@gmail.com>
2026-03-11 15:32:15 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_126621_RevandAnrianPutra/week06/Main.kt
+++ src/oop_126621_RevandAnrianPutra/week06/Main.kt
@@ -18,4 +18,8 @@ fun main() {
     println("\n=== TESTING CHECKOUT ===")
     processCheckout(method = pay1, amount = 50000.0)
     processCheckout(method = pay2, amount = 150000.0)
+
+    val lamp = SmartLamp(id = "L01", name = "Ruang Tamu")
+    val speaker = SmartSpeaker(id = "S01", name = "Google Nest Dapur")
+    val cctv = SmartCCTV(id = "C01", name = "Ezviz Garasi")
 }
