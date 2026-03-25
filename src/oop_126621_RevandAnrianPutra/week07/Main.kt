ac1cd8a389f6a57473e5b85d37112b4f6094d71e
Revlog15 <revand.anrian.putra@gmail.com>
2026-03-25 15:59:37 +0700

    week07: implement data class for structural equality

--- src/oop_126621_RevandAnrianPutra/week07/Main.kt
+++ src/oop_126621_RevandAnrianPutra/week07/Main.kt
@@ -16,4 +16,10 @@ fun main() {
     println(reg1) // Akan mencetak memori hash
     println("Sama? ${reg1 == reg2}") // False
 
+    println("\n=== TEST DATA CLASS ===")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(data1) // Otomatis readable format
+    println("Sama? ${data1 == data2}") // True (Structural Equality)
+
 }
