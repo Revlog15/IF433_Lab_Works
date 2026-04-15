76e7b2f1d359357adc2d223014052741f54272a8
Revlog15 <revand.anrian.putra@gmail.com>
2026-04-15 15:33:21 +0700

    week08: (task) implement payment processing with !!

--- src/oop_126621_RevandAnrianPutra/week08/JavaPaymentService.java
+++ src/oop_126621_RevandAnrianPutra/week08/JavaPaymentService.java
@@ -1,4 +1,9 @@
 package oop_126621_RevandAnrianPutra.week08;
 
 public class JavaPaymentService {
+    public class JavaPaymentService {
+        public static String processPayment(String productId) {
+            return "TRX-" + productId + "-SUCCESS";
+        }
+    }
 }
