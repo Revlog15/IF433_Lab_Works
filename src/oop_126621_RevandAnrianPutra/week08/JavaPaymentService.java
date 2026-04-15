23006dcffdc05df5371617379422423d637d3f9e
Revlog15 <revand.anrian.putra@gmail.com>
2026-04-15 15:40:02 +0700

    week08: (task) test full e-commerce pipeline in main

--- src/oop_126621_RevandAnrianPutra/week08/JavaPaymentService.java
+++ src/oop_126621_RevandAnrianPutra/week08/JavaPaymentService.java
@@ -1,9 +1,8 @@
 package oop_126621_RevandAnrianPutra.week08;
 
-public class JavaPaymentService {
     public class JavaPaymentService {
         public static String processPayment(String productId) {
             return "TRX-" + productId + "-SUCCESS";
         }
     }
-}
+
