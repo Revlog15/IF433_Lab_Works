748e33e50754a41855ab4bbd88ecd57d00372459
Revlog15 <revand.anrian.putra@gmail.com>
2026-04-15 15:29:19 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

--- src/oop_126621_RevandAnrianPutra/week08/EcommerceModels.kt
+++ src/oop_126621_RevandAnrianPutra/week08/EcommerceModels.kt
@@ -1,2 +1,5 @@
 package oop_126621_RevandAnrianPutra.week08
 
+sealed class Product
+data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+data class Clothing(val id: String, val name: String, val size: String) : Product()
