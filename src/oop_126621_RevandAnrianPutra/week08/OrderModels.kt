09144dee8d9af4316a43300903b29b9b5fc689db
Revlog15 <revand.anrian.putra@gmail.com>
2026-04-15 15:19:38 +0700

    week08: implement safe call with let block

--- src/oop_126621_RevandAnrianPutra/week08/OrderModels.kt
+++ src/oop_126621_RevandAnrianPutra/week08/OrderModels.kt
@@ -1,2 +1,6 @@
 package oop_126621_RevandAnrianPutra.week08
 
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
