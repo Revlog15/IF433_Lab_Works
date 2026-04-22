b416595e9d36c1b3e1929123774b572d4d6adb32
Revlog15 <revand.anrian.putra@gmail.com>
2026-04-22 15:55:02 +0700

    week09: create immutable and mutable list(2 bcs of error)

--- src/oop_126621_RevandAnrianPutra/week09/CollectionsBasics.kt
+++ src/oop_126621_RevandAnrianPutra/week09/CollectionsBasics.kt
@@ -2,24 +2,11 @@ package oop_126621_RevandAnrianPutra.week09
 
 fun main() {
     println("=== TEST LIST ===")
-    // Immutable List: Tidak bisa diubah setelah dibuat
-    val frameworks: List<String> = listOf("Kotlin", "Java", "C++") [cite: 17]
-    // frameworks.add("Python") // UNCOMMENT INI AKAN ERROR! [cite: 18]
-    println("Immutable List: $frameworks") [cite: 19]
+    val frameworks: List<String> = listOf("Kotlin", "Java", "C++")
+    // frameworks.add("Python") // UNCOMMENT INI AKAN ERROR!
+    println("Immutable List: $frameworks")
 
-    // Mutable List: Bisa ditambah/dikurangi
-    val scores: MutableList<Int> = mutableListOf(85, 90) [cite: 21]
-    scores.add(95) [cite: 22]
-    scores[0] = 88 [cite: 23]
-    println("Mutable List: $scores") [cite: 24]
-
-    println("\n=== TEST SET ===") [cite: 30]
-    val uniqueNumbers = setOf(1, 2, 2, 3, 3, 4) [cite: 31]
-    println("Unique Numbers (Set): $uniqueNumbers") [cite: 32]
-
-    val activeUsers = mutableSetOf("UserA", "UserB") [cite: 33]
-    activeUsers.add("UserC") [cite: 34]
-    activeUsers.add("UserA") // Diabaikan karena sudah ada [cite: 35]
-    println("Active Users: $activeUsers")
-
-}
+    val scores: MutableList<Int> = mutableListOf(85, 90)
+    scores.add(95)
+    scores[0] = 88
+    println("Mutable List: $scores")
