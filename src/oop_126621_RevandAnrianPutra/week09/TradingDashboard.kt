b48bee3346e0b9ddd6e0214e60b898fc7adb0316
Revlog15 <revand.anrian.putra@gmail.com>
2026-04-22 16:28:36 +0700

    week09: (task) chain sortedBy and map for worst performers

--- src/oop_126621_RevandAnrianPutra/week09/TradingDashboard.kt
+++ src/oop_126621_RevandAnrianPutra/week09/TradingDashboard.kt
@@ -18,3 +18,7 @@ fun main() {
     val topPerformersString = winningTrades
         .sortedByDescending { it.roe }
         .map { "WIN [${it.pair} ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
+
+    val worstPerformersString = losingTrades
+        .sortedBy { it.roe } // Diurutkan Ascending (dari minus terbesar ke terkecil)
+        .map { "LOSS [${it.pair} ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
