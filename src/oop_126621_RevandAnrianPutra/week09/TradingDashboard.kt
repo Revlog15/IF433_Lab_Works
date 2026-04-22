9f402f64c87c365e79619f243478bd6a4dac8391
Revlog15 <revand.anrian.putra@gmail.com>
2026-04-22 16:25:21 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_126621_RevandAnrianPutra/week09/TradingDashboard.kt
@@ -0,0 +1,11 @@
+package oop_126621_RevandAnrianPutra.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 5, 2.0, "OPEN"),
+        TradeLog("BTCUSDT", "SHORT", 20, -1.5, "CLOSED"),
+        TradeLog("ADAUSDT", "LONG", 10, 8.0, "CLOSED"),
+        TradeLog("BNBUSDT", "SHORT", 15, 0.0, "OPEN")
+    )
