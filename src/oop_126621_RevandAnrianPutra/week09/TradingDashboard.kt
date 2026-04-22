c60a2e4ce5c2c3b21091b324abab5cce05325126
Revlog15 <revand.anrian.putra@gmail.com>
2026-04-22 16:29:24 +0700

    week09: (task) print top performers using forEach

--- src/oop_126621_RevandAnrianPutra/week09/TradingDashboard.kt
+++ src/oop_126621_RevandAnrianPutra/week09/TradingDashboard.kt
@@ -24,3 +24,6 @@ fun main() {
         .map { "LOSS [${it.pair} ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
 
     val uniquePairs = tradeHistory.map { it.pair }.toSet()
+
+    println("\n[ Top Performers ]")
+    topPerformersString.forEach { println(it) }
