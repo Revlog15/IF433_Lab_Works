c614938c259e8f64cce58f4c3d77c857aa711154
Revlog15 <revand.anrian.putra@gmail.com>
2026-04-29 23:54:24 +0700

    week10: (task) populate coin repository

--- src/oop_126621_RevandAnrianPutra/week10/CryptoDashboard.kt
+++ src/oop_126621_RevandAnrianPutra/week10/CryptoDashboard.kt
@@ -2,3 +2,7 @@ package oop_126621_RevandAnrianPutra.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 5000.0))
