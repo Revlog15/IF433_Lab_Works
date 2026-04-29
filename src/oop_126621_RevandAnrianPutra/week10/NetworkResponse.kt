166c7996424ceeea6bac1b369be092ad62a97305
Revlog15 <revand.anrian.putra@gmail.com>
2026-04-29 23:52:37 +0700

    week10: (task) implement search logic in repository

--- src/oop_126621_RevandAnrianPutra/week10/NetworkResponse.kt
+++ src/oop_126621_RevandAnrianPutra/week10/NetworkResponse.kt
@@ -1,3 +1,7 @@
 package oop_126621_RevandAnrianPutra.week10
 
 data class ApiResponse<T>(val status: String, val data: T)
+
+fun <T> search(items: List<T>, keyword: String, selector: (T) -> String): List<T> {
+    return items.filter { selector(it).contains(keyword, ignoreCase = true) }
+}
