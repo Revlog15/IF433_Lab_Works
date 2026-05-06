35157bdd9b273ae939bf95cc6e38a4a0bcbadb3e
Revlog15 <revand.anrian.putra@gmail.com>
2026-05-06 10:37:37 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_126621_RevandAnrianPutra/week11/SmartHomeModels.kt
+++ src/oop_126621_RevandAnrianPutra/week11/SmartHomeModels.kt
@@ -1,2 +1,12 @@
 package oop_126621_RevandAnrianPutra.week11
 
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
+
+fun SmartDevice.diagnose(): String {
+    return "[DIAGNOSTIK] $name | Kategori: $category | Status: ${if (isOnline) "Online" else "Offline"} | Daya: $powerLoad Watt"
+}
