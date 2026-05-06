6cd3bc3834bd3e0e01453a80375721467044932c
Revlog15 <revand.anrian.putra@gmail.com>
2026-05-06 10:34:23 +0700

    week11: implement run function for object computation

--- src/oop_126621_RevandAnrianPutra/week11/Main.kt
+++ src/oop_126621_RevandAnrianPutra/week11/Main.kt
@@ -1,2 +1,10 @@
 package oop_126621_RevandAnrianPutra.week11
 
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
