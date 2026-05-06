fe3241b67e4a3534c710420cf55929b066a1b06c
Revlog15 <revand.anrian.putra@gmail.com>
2026-05-06 10:31:49 +0700

    week11: create nullable receiver extension

--- src/oop_126621_RevandAnrianPutra/week11/StringExtensions.kt
+++ src/oop_126621_RevandAnrianPutra/week11/StringExtensions.kt
@@ -1,2 +1,13 @@
 package oop_126621_RevandAnrianPutra.week11
 
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
+
+fun String?.isNullOrEmptyCustom(): Boolean {
+    return this == null || this.isEmpty()
+}
