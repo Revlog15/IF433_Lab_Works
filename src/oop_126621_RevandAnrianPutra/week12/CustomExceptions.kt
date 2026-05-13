c02d6b0aef31adc92ada8a1449b9b093ce400cd3
Revlog15 <revand.anrian.putra@gmail.com>
2026-05-13 15:39:14 +0700

    week12: create InsufficientFunds exception

---
+++ src/oop_126621_RevandAnrianPutra/week12/CustomExceptions.kt
@@ -0,0 +1,6 @@
+package oop_126621_RevandAnrianPutra.week12
+
+class InsufficientFundsException(
+    val amount: Double,
+    val balance: Double
+) : Exception("Attempted $amount, balance: $balance")
