ce6286eb854dc4c15975671f4e842e83fcb352f8
Revlog15 <revand.anrian.putra@gmail.com>
2026-05-21 23:23:25 +0700

    week13: implement printWriter with safe use block

--- src/oop_126621_RevandAnrianPutra/week13/ResourceHandling.kt
+++ src/oop_126621_RevandAnrianPutra/week13/ResourceHandling.kt
@@ -7,18 +7,24 @@ fun main() {
 
     val unsafeFile = File("unsafe_logs.txt")
 
-    // Membuka stream secara manual
     val writer = unsafeFile.printWriter()
 
     writer.println("Log 1: Membuka koneksi database...")
     writer.println("Log 2: Menulis data pengguna...")
 
-    // BAHAYA:
-    // Jika terjadi exception sebelum close(),
-    // resource bisa tidak tertutup dengan benar.
-
-    // Wajib dipanggil manual jika tidak memakai blok use
     writer.close()
 
     println("Proses penulisan unsafe selesai.")
+
+    println("\n=== TEST SAFE RESOURCE HANDLING ===")
+
+    val safeFile = File("safe_logs.txt")
+
+    safeFile.printWriter().use { out ->
+        for (i in 1..100) {
+            out.println("Safe Log entry #$i: System status OK.")
+        }
+    }
+
+    println("100 baris log berhasil di-generate dengan sangat aman.")
 }
