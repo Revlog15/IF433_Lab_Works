d3514a8fc5a895237cfcd4ad25f9198266bad559
Revlog15 <revand.anrian.putra@gmail.com>
2026-05-21 23:25:56 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_126621_RevandAnrianPutra/week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_126621_RevandAnrianPutra.week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
