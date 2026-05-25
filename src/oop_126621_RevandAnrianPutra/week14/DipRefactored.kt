442f7dfca8fede92b0d5cc1891f7c3cdd1f9978e
Revlog15 <revand.anrian.putra@gmail.com>
2026-05-25 10:40:02 +0700

    week14: inject Database abstraction to fix DIP

--- src/oop_126621_RevandAnrianPutra/week14/DipRefactored.kt
+++ src/oop_126621_RevandAnrianPutra/week14/DipRefactored.kt
@@ -1,2 +1,33 @@
 package oop_126621_RevandAnrianPutra.week14
 
+interface Database {
+    fun query(sql: String): List<String>
+}
+
+class PostgreDatabase : Database {
+    override fun query(sql: String): List<String> {
+        return listOf("pg_data1", "pg_data2")
+    }
+}
+
+class MongoDatabase : Database {
+    override fun query(sql: String): List<String> {
+        return listOf("mongo_data1", "mongo_data2")
+    }
+}
+
+class SafeUserService(
+    private val db: Database
+) {
+    fun getUser(id: Int): List<String> {
+        return db.query("SELECT * FROM users WHERE id=$id")
+    }
+}
+
+fun main() {
+    val postgreService = SafeUserService(PostgreDatabase())
+    val mongoService = SafeUserService(MongoDatabase())
+
+    println(postgreService.getUser(1))
+    println(mongoService.getUser(2))
+}
--- src/oop_126621_RevandAnrianPutra/week14/DipViolation.kt
+++ src/oop_126621_RevandAnrianPutra/week14/DipViolation.kt
@@ -1,2 +1,20 @@
 package oop_126621_RevandAnrianPutra.week14
 
+class MySQLDatabase {
+    fun query(sql: String): List<String> {
+        return listOf("data1", "data2")
+    }
+}
+
+class UserService {
+    private val database = MySQLDatabase()
+
+    fun getUser(id: Int): List<String> {
+        return database.query("SELECT * FROM users WHERE id=$id")
+    }
+}
+
+fun main() {
+    val service = UserService()
+    println(service.getUser(1))
+}
