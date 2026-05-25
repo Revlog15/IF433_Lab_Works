7a734ceab757553d744f991f32a6febb4f4f1954
Revlog15 <revand.anrian.putra@gmail.com>
2026-05-25 10:28:48 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_126621_RevandAnrianPutra/week14/OcpRefactored.kt
@@ -0,0 +1,17 @@
+package oop_126621_RevandAnrianPutra.week14
+
+interface DiscountStrategy {
+    fun apply(price: Double): Double
+}
+
+class StudentDiscount : DiscountStrategy {
+    override fun apply(price: Double): Double {
+        return price * 0.80
+    }
+}
+
+class MemberDiscount : DiscountStrategy {
+    override fun apply(price: Double): Double {
+        return price * 0.85
+    }
+}
